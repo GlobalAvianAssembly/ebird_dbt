bc38d55639eba375108db1e737630f8c65f28b9f
James <jamese.richardson@autotrader.co.uk>
2020-12-23 17:02:03 +0000

    Adding observation view

---
+++ models/ebird/observation.sql
@@ -0,0 +1,12 @@
+SELECT
+    global_unique_identifier AS observation_id,
+    scientific_name,
+    subspecies_scientific_name,
+    common_name,
+    subspecies_common_name,
+    observation_count,
+    sampling_event_identifier AS checklist_id
+FROM {{ source("dropbox", "ebird") }}
+WHERE sampling_event_identifier IN
+    (SELECT checklist_id FROM {{ ref("checklist") }})
+    AND approved = 1
