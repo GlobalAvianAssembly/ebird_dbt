d35c21d5d023e39984a38336486b83b1f221993b
James <jamese.richardson@autotrader.co.uk>
2020-12-23 16:47:21 +0000

    Adding first two models for hotspot and checklist

---
+++ models/ebird/checklist.sql
@@ -0,0 +1,30 @@
+
+WITH checklists AS (
+    SELECT
+        sampling_event_identifier AS checklist_id,
+        locality_id,
+        number_observers,
+        group_identifier,
+        observation_date,
+        time_observations_started,
+        effort_distance_km,
+        effort_area_ha,
+        duration_minutes,
+        protocol_type,
+        row_number() OVER (PARTITION BY sampling_event_identifier) AS checklist_id_rownum
+    FROM {{ source("dropbox", "ebird") }}
+    WHERE all_species_reported = 1
+        AND protocol_type IN ('Traveling', 'Stationary', 'Area')
+        AND locality_id IN (SELECT locality_id FROM {{ ref("hotspot") }})
+),
+checklist_deduplicate AS (
+  SELECT
+    * EXCEPT (checklist_id_rownum, group_identifier),
+    row_number() OVER (PARTITION BY group_identifier) AS group_identifier_rownum
+  FROM checklists
+  WHERE checklist_id_rownum = 1
+)
+SELECT
+    * EXCEPT (group_identifier_rownum)
+FROM checklist_deduplicate
+WHERE group_identifier_rownum = 1
---
+++ models/ebird/hotspot.sql
@@ -0,0 +1,17 @@
+WITH hotspots AS (
+    SELECT
+        locality_id,
+        locality as name,
+        latitude,
+        longitude,
+        county_code,
+        state_code,
+        country_code,
+        row_number() OVER (PARTITION BY locality_id) AS locality_id_rownum
+    FROM {{ source("dropbox", "ebird") }}
+    WHERE locality_type = 'H'
+)
+SELECT
+    * EXCEPT(locality_id_rownum)
+FROM hotspots
+WHERE locality_id_rownum = 1
