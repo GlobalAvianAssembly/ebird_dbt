b721c121b4a79a1f7c4b02bd8413aa7b733f0344
James <jamese.richardson@autotrader.co.uk>
2021-01-16 22:47:43 +0000

    Add number of checklists a species appears on within a city

--- models/model/regional_species.sql
+++ models/model/regional_species.sql
@@ -2,7 +2,8 @@ SELECT
     species.city_id AS city_id,
     common_name,
     scientific_name,
-    ROUND(AVG(percentage_of_checklists), 2) AS average_percentage_of_checklists
+    ROUND(AVG(percentage_of_checklists), 2) AS average_percentage_of_checklists,
+    COUNT(*) AS number_of_checklists_appearances
 FROM {{ ref('eph_included_species_at_hotspot') }} species
 JOIN {{ ref('eph_included_hotspot') }} USING(locality_id)
 WHERE {{ is_regional() }}
