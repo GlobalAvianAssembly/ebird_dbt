9fd4bd9caaa23ced9eddc0a12464c88e05a830b4
James <jamese.richardson@autotrader.co.uk>
2021-01-17 08:35:37 +0000

    Fix number of hotspot appearaces in species pool

--- models/model/regional_species.sql
+++ models/model/regional_species.sql
@@ -3,7 +3,7 @@ SELECT
     common_name,
     scientific_name,
     ROUND(AVG(percentage_of_checklists), 2) AS average_percentage_of_checklists,
-    COUNT(*) AS number_of_hotspot_appearances
+    COUNT(DISTINCT locality_id) AS number_of_hotspot_appearances
 FROM {{ ref('eph_included_species_at_hotspot') }} species
 JOIN {{ ref('eph_included_hotspot') }} USING(locality_id)
 WHERE {{ is_regional() }}
