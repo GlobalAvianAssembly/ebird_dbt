17dfdf2aea8df82cde9c7b50d2a7fae06dfeedcb
James <jamese.richardson@autotrader.co.uk>
2021-04-07 17:40:34 +0100

    Add the origin, presence, and seasonal fields from birdlife to regional pools
    And exclude extinct birds

--- models/intermediate/int_birdlife_regional_species_pool.sql
+++ models/intermediate/int_birdlife_regional_species_pool.sql
@@ -1,9 +1,39 @@
 SELECT DISTINCT
-    regional_pool.binomial AS scientific_name,
+    regional_pool.species AS scientific_name,
     t.common_name AS common_name,
-    city.city_id AS city_id
+    city.city_id AS city_id,
+    CASE regional_pool.presence
+        WHEN '1' THEN 'Extant'
+        WHEN '2' THEN 'Probably Extant'
+        WHEN '3' THEN 'Possibly Extant'
+        WHEN '4' THEN 'Possibly Extinct'
+        WHEN '5' THEN 'Extinct'
+        WHEN '6' THEN 'Presence Uncertain'
+        ELSE 'Unknown Code'
+    END AS presence,
+    regional_pool.presence AS presence_code,
+    CASE regional_pool.seasonal
+        WHEN '1' THEN 'Resident'
+        WHEN '2' THEN 'Breeding Season'
+        WHEN '3' THEN 'Non-breeding Season'
+        WHEN '4' THEN 'Passage'
+        WHEN '5' THEN 'Seasonal occurence uncertain'
+        ELSE 'Unknown Code'
+    END AS seasonal,
+    regional_pool.seasonal AS seasonal_code,
+     CASE regional_pool.origin
+        WHEN '1' THEN 'Native'
+        WHEN '2' THEN 'Reintroduced'
+        WHEN '3' THEN 'Introduced'
+        WHEN '4' THEN 'Vagrant'
+        WHEN '5' THEN 'Origin Uncertain'
+        WHEN '6' THEN 'Assisted Colonisation'
+        ELSE 'Unknown Code'
+    END AS origin,
+    regional_pool.origin AS origin_code
 FROM {{ source('dropbox', 'ee_birdlife_distribution_intersection_with_urban_area') }} regional_pool
 LEFT JOIN {{ ref('int_birdlife_taxonomy') }} t
-    ON regional_pool.binomial = t.scientific_name
+    ON regional_pool.species = t.scientific_name
 JOIN {{ ref('city') }} city
     ON regional_pool.city_name = city.name
+WHERE regional_pool.presence != '5'
--- models/model/regional_species.sql
+++ models/model/regional_species.sql
@@ -8,14 +8,20 @@ ebird AS (
 birdlife AS (
     SELECT
         scientific_name,
-        city_id
+        city_id,
+        seasonal,
+        seasonal_code,
+        presence,
+        presence_code,
+        origin,
+        origin_code
     FROM {{ ref('int_birdlife_regional_species_pool') }}
 ),
 all_species AS (
     SELECT DISTINCT
         scientific_name,
         city_id
-    FROM (SELECT * FROM ebird UNION ALL SELECT * FROM birdlife)
+    FROM (SELECT * FROM ebird UNION ALL SELECT scientific_name, city_id FROM birdlife)
 ),
 species_pools AS (
     SELECT
@@ -34,11 +40,19 @@ SELECT
     species_pools.city_id AS city_id,
     species_pools.present_in_ebird AS present_in_ebird,
     species_pools.present_in_birdlife AS present_in_birdlife,
-    COALESCE(ebird.number_of_hotspot_appearances, 0) AS number_of_ebird_hotspot_appearances
+    COALESCE(ebird.number_of_hotspot_appearances, 0) AS number_of_ebird_hotspot_appearances,
+    birdlife.seasonal AS birdlife_seasonal,
+    birdlife.presence AS birdlife_presence,
+    birdlife.origin AS birdlife_origin,
+    birdlife.seasonal_code AS birdlife_seasonal_code,
+    birdlife.presence_code AS birdlife_presence_code,
+    birdlife.origin_code AS birdlife_origin_code
 FROM species_pools
 LEFT JOIN {{ ref('int_birdlife_taxonomy') }} t
     ON species_pools.scientific_name = t.scientific_name
 LEFT JOIN {{ ref('int_ebird_regional_species_pool') }} ebird
     ON species_pools.scientific_name = ebird.scientific_name AND species_pools.city_id = ebird.city_id
+LEFT JOIN {{ ref('int_birdlife_regional_species_pool') }} birdlife
+    ON species_pools.scientific_name = birdlife.scientific_name AND species_pools.city_id = birdlife.city_id
 ORDER BY present_in_ebird DESC, present_in_birdlife DESC, species_pools.scientific_name
 
