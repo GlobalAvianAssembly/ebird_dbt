3b34fd55f21a0487d65ce400ab8fe7b11ebab2e1
James <jamese.richardson@autotrader.co.uk>
2021-05-15 15:18:11 +0100

    Model out urban area using birdlife regional pool and include city data

--- models/model/urban_area.sql
+++ models/model/urban_area.sql
@@ -9,22 +9,13 @@ WITH urban_hotspots AS (
     FROM {{ ref('eph_included_hotspot') }}
     WHERE {{ is_urban() }}
     GROUP BY city_id
-), buffer_hotspots AS (
-    SELECT
-        city_id,
-        COUNT(*) AS total_regional_hotspots,
-        MAX(elevation) AS max_regional_hotspot_elevation,
-        MIN(elevation) AS min_regional_hotspot_elevation
-    FROM {{ ref('eph_included_hotspot') }}
-    WHERE {{ is_regional() }}
-    GROUP BY city_id
 ), urban_species AS (
     SELECT
         city_id,
         common_name,
         COUNT(*) AS number_of_hotspot_appearances
-    FROM {{ ref('eph_included_species_at_hotspot') }}
-    WHERE locality_id IN (SELECT locality_id FROM {{ ref('eph_included_hotspot') }} WHERE {{ is_urban() }})
+    FROM {{ ref('urban_species_birdlife') }}
+    WHERE present_in_regional_pool = TRUE
     GROUP BY city_id, common_name
 ), urban_richness AS (
     SELECT
@@ -37,38 +28,84 @@ WITH urban_hotspots AS (
 ), regional_richness AS (
     SELECT
         city_id,
-        COUNT(DISTINCT scientific_name) AS regional_richness,
-        COUNT(IF(number_of_ebird_hotspot_appearances = 1, 1, NULL)) AS one_observation,
-        COUNT(IF(number_of_ebird_hotspot_appearances = 2, 1, NULL)) AS two_observations
+        COUNT(DISTINCT scientific_name) AS species_in_regional_pool
     FROM {{ ref('regional_species') }}
-    WHERE present_in_birdlife = TRUE AND present_in_ebird = TRUE
+    WHERE present_in_birdlife = TRUE
     GROUP BY city_id
+), city_landcover AS (
+    SELECT
+        city_id,
+        STRUCT(
+            city_pc_bare AS bare,
+            STRUCT(
+                city_pc_closed_forest_deciduous_broadleaf AS deciduous_broadleaf,
+                city_pc_closed_forest_deciduous_needle AS deciduous_needle,
+                city_pc_closed_forest_evergreen_broadleaf AS evergreen_broadleaf,
+                city_pc_closed_forest_evergreen_needle AS evergeen_needle,
+                city_pc_closed_forest_forest_mixed AS mixed,
+                city_pc_closed_forest_forest_other AS other
+            ) AS closed_forest_cover,
+            city_pc_closed_forest_forest_total AS closed_forest_total,
+            city_pc_cultivated AS cultivated,
+            city_pc_herbaceous_vegetation AS herbaceous_vegetation,
+            city_pc_herbaceous_wetland AS herbaceous_wetland,
+            city_pc_moss_and_lichen AS moss_and_lichen,
+            city_pc_ocean AS ocean,
+            STRUCT(
+                city_pc_open_forest_deciduous_broadleaf AS deciduous_broadleaf,
+                city_pc_open_forest_deciduous_needle AS deciduous_needle,
+                city_pc_open_forest_evergreen_broadleaf AS evergreen_broadleaf,
+                city_pc_open_forest_evergreen_needle AS evergeen_needle,
+                city_pc_open_forest_forest_mixed AS mixed,
+                city_pc_open_forest_forest_other AS other
+            ) AS open_forest_cover,
+            city_pc_open_forest_forest_total AS open_forest_total,
+            city_pc_permanent_water AS permanent_water,
+            city_pc_shrubs AS shrubs,
+            city_pc_snow AS snow,
+            city_pc_unknown AS unknown,
+            city_pc_urban AS urban
+        ) AS percentage_landcover,
+        pop_2015 AS population_in_2015,
+        city_calcuated_area AS total_area,
+        STRUCT(
+            max_elevation AS max,
+            min_elevation AS min,
+            max_elevation - min_elevation AS delta
+        ) AS elevation,
+        distance_to_coast
+    FROM
+        {{ source ('dropbox', 'ee_city_copernicus_land_coverage') }} data
+    JOIN
+        {{ source ('dropbox', 'ee_city_elevation_delta') }} elevation_data USING (city_name)
+    JOIN
+        {{ source ('dropbox', 'ee_city_distance_to_coastline') }} coast_distance USING (city_name)
+    JOIN
+        {{ ref('city') }} city ON city.name = data.city_name
 )
 SELECT
     city.city_id,
     city.name,
-    city.population_2015,
-    city.built_area_2015,
-    city.latitude,
-    city.longitude,
-    urban_hotspots.total_urban_hotspots AS total_urban_hotspots,
+    STRUCT(
+        city.latitude,
+        city.longitude
+    ) AS location,
+    regional_richness.species_in_regional_pool AS species_in_regional_pool,
     urban_richness.urban_richness AS urban_richness,
     CASE
         WHEN urban_richness.two_observations > 0
         THEN ROUND(urban_richness.urban_richness + ((urban_richness.one_observation * urban_richness.one_observation) / (2 * urban_richness.two_observations)), 1)
         ELSE -1
     END AS urban_chao_estimate,
-    urban_hotspots.min_urban_hotspot_elevation AS min_urban_hotspot_elevation,
-    urban_hotspots.max_urban_hotspot_elevation AS max_urban_hotspot_elevation,
-    regional_richness.regional_richness AS regional_richness,
-    CASE
-        WHEN regional_richness.two_observations > 0
-        THEN ROUND(regional_richness.regional_richness + ((regional_richness.one_observation * regional_richness.one_observation) / (2 * regional_richness.two_observations)), 1)
-        ELSE -1
-    END AS regional_chao_estimate,
-    (SELECT COUNT(*) FROM {{ ref('regional_species') }} bl WHERE bl.present_in_birdlife = TRUE AND bl.city_id = city.city_id) AS birdlife_regional_richness
+    ROUND(urban_richness.urban_richness / regional_richness.species_in_regional_pool * 100, 1) AS percentage_of_regional_richness,
+    STRUCT(
+        urban_hotspots.min_urban_hotspot_elevation AS min_elevation,
+        urban_hotspots.max_urban_hotspot_elevation AS max_elevation,
+        urban_hotspots.total_urban_hotspots AS count
+    ) AS urban_hotspots,
+    city_landcover.* EXCEPT (city_id)
 FROM {{ ref ('city') }} city
 JOIN urban_hotspots USING (city_id)
-JOIN buffer_hotspots USING (city_id)
 JOIN urban_richness USING (city_id)
 JOIN regional_richness USING (city_id)
+JOIN city_landcover USING(city_id)
