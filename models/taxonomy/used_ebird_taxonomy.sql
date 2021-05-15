8fb6f60644af2904107745921da579c8c61e7f69
James <jamese.richardson@autotrader.co.uk>
2021-05-15 13:42:25 +0100

    Ensuring all urban ebird data is mapped to a birdlife species

--- models/taxonomy/used_ebird_taxonomy.sql
+++ models/taxonomy/used_ebird_taxonomy.sql
@@ -22,6 +22,17 @@ WITH distinct_ebird_species AS (
         JOIN distinct_ebird_species ebird ON ebird.scientific_name = alternative_scientific_name
 ), species_not_in_birdlife AS (
     SELECT
+        distinct_ebird_species.scientific_name AS scientific_name,
+        distinct_ebird_species.common_name AS common_name,
+        manual_mapping.birdlife_scientific_name AS birdlife_scientific_name,
+        birdlife.common_name AS birdlife_common_name
+    FROM distinct_ebird_species
+        JOIN {{ source('dropbox', 'avibase_mapped_ebird_taxonomy_missing_from_birdlife') }} manual_mapping
+            ON distinct_ebird_species.scientific_name = manual_mapping.scientific_name
+        JOIN {{ ref('birdlife_taxonomy_with_alternatives') }} birdlife
+            ON birdlife.scientific_name = manual_mapping.birdlife_scientific_name
+), unmapped_species AS (
+    SELECT
         scientific_name,
         common_name,
         CAST(NULL AS STRING) AS birdlife_scientific_name,
@@ -29,12 +40,15 @@ WITH distinct_ebird_species AS (
     FROM distinct_ebird_species
     WHERE scientific_name NOT IN (SELECT scientific_name FROM species_with_same_binomial_as_birdlife)
         AND scientific_name NOT IN (SELECT scientific_name FROM species_with_alternative_binomial_in_birdlife)
+        AND scientific_name NOT IN (SELECT scientific_name FROM species_not_in_birdlife)
 ), used_ebird_species AS (
     SELECT * FROM species_with_same_binomial_as_birdlife
     UNION ALL
     SELECT * FROM species_with_alternative_binomial_in_birdlife
     UNION ALL
     SELECT * FROM species_not_in_birdlife
+    UNION ALL
+    SELECT * FROM unmapped_species
 )
 SELECT * FROM used_ebird_species ORDER BY scientific_name
 
