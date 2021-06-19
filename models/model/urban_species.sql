87ee84b31be354689cb18230f567f612b3c6fa36
James Richardson <jamese.richardson@autotrader.co.uk>
2021-06-19 15:11:16 +0100

    Run urban_species model

--- models/model/urban_species.sql
+++ models/model/urban_species.sql
@@ -3,12 +3,12 @@ SELECT
     hotspot_id,
     species.locality_id AS locality_id,
     species.city_id AS city_id,
-    common_name,
-    scientific_name,
+    species.common_name,
+    species.scientific_name,
     birdlife_scientific_name,
     birdlife_common_name,
     percentage_of_checklists
 FROM {{ ref('eph_included_species_at_hotspot') }} species
 JOIN {{ ref('eph_included_hotspot') }} USING (hotspot_id)
-JOIN {{ ref('used_ebird_species') }}  USING (scientific_name)
+JOIN {{ ref('used_ebird_taxonomy') }}  USING (scientific_name)
 WHERE {{ is_urban() }}
