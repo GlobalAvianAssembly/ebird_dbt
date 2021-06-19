3406100f9194704d28845420d80011c994cb56bc
James <james@Jamess-iMac.fritz.box>
2021-06-19 15:06:57 +0100

    Include birdlife naming in urban_species

--- models/model/urban_species.sql
+++ models/model/urban_species.sql
@@ -5,7 +5,10 @@ SELECT
     species.city_id AS city_id,
     common_name,
     scientific_name,
+    birdlife_scientific_name,
+    birdlife_common_name,
     percentage_of_checklists
 FROM {{ ref('eph_included_species_at_hotspot') }} species
 JOIN {{ ref('eph_included_hotspot') }} USING (hotspot_id)
+JOIN {{ ref('used_ebird_species') }}  USING (scientific_name)
 WHERE {{ is_urban() }}
