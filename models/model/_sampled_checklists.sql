2eecba1c861b5ba15740bf41b15c87acd677d976
James <james@Jamess-iMac.fritz.box>
2021-08-08 15:52:43 +0100

    remove sampled models from model

--- models/model/_sampled_checklists.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='incremental') }}
-
-SELECT
-    0 AS sample_id,
-    '' AS scientific_name,
-    '' AS common_name,
-    0.0 AS percentage_of_checklists,
-    '' AS locality_id
-LIMIT 0
